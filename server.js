48223b105765b644aff4f3c8d58e0d393805a832
unknown <coolsohaib602@gmail.com>
2020-07-12 07:23:22 +0530

    docker files removed

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 'use strict';
 const express = require('express');
 // Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const PORT = 5000;
+const HOST = 'localhost';
 // App
 const app = express();
 app.get('/', (req, res) => {
